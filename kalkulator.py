8551f3d6750d64a2d425efbe40596d99ce5e7efa
StanislawKiraga <kiragasta@gmail.com>
2023-02-23 14:56:31 +0100

    add functions

--- kalkulator.py
+++ kalkulator.py
@@ -10,30 +10,40 @@ Podaj działanie jakie chcesz wykonać, wybierając odpowiednią liczbę:
 Twój wybór: '''))
 
 if choice < 1 or choice > 4:
-    print('Wybór nieprawidłowy')
+    logging.error('Wybór nieprawidłowy')
     exit(1)
 
 num1 = float(input('Podaj pierwszą wartość: '))
 num2 = float(input('Podaj drugą wartość: '))
 
+def sum(x, y):
+    result = x + y
+    print(f'Wynik to: {result}')
+
+def difr(x, y):
+    result = num1 - num2
+    print(f'Wynik to: {result}')
+
+def iloczyn(x, y):
+    result = num1 * num2
+    print(f'Wynik to: {result}')
+
+def iloraz(x, y):
+    result = num1 / num2
+    print(f'Wynik to: {result}')
+
 if choice == 1:
-    sum = num1 + num2
     logging.info(f'Dodaję {num1} + {num2}')
-    print(f'Wynik to: {sum}')
-
-if choice == 2:
-    difr = num1 - num2
+    sum(num1, num2)
+    
+if choice == 2:    
     logging.info(f'Odejmuję {num1} - {num2}')
-    print(f'Wynik to: {difr}')
-
-if choice == 3:
-    iloczyn = num1 * num2
+    difr(num1, num2)
+    
+if choice == 3:    
     logging.info(f'Mnozę {num1} * {num2}')
-    print(f'Wynik to: {iloczyn}')
+    iloczyn(num1, num2)
 
-if choice == 4:
-    iloraz = num1 / num2
+if choice == 4:    
     logging.info(f'Dzielę {num1} / {num2}')
-    print(f'Wynik to: {iloraz}')
-
-logging.info('Koniec')
+    iloraz(num1, num2)
